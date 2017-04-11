2e95dabb42f61e132b8c0b7524b86e453708c809
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-11 00:21:41 +0000

    created the request URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,11 @@
 var request = require('request');
-
-console.log('Welcome to the GitHub Avatar Downloader!');
+var GITHUB_USER = "raz346";
+var GITHUB_TOKEN = "b086bcc6763f5fbb966f33c90ec5f24e7a2dd019";
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
