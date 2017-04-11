bbc1ebdcdd13f8af38d6b66aed152d843dbce3fd
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-11 01:08:12 +0000

    request the data body

--- download_avatars.js
+++ download_avatars.js
@@ -3,9 +3,23 @@ var GITHUB_USER = "raz346";
 var GITHUB_TOKEN = "b086bcc6763f5fbb966f33c90ec5f24e7a2dd019";
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
+  //  define user-agent 
+  var requestOption = {
+
+    headers : {
+      "user-agent" : "Ahmed"
+    },
+    url : requestURL 
+  }
+  // handle error
+  request.get(requestOption,cb)
+       .on('error', function (err) {                                    
+         console.log(err);
+       })
+      //  handle the data
+      .on('response', function (response) {                         
+       })
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log(result.body);   
 });
