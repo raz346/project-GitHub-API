864528b4dbe97646b33bf0db01c88b140ceddd0e
Ahmed Al-Hashimi <Alhashimi346@gmail.com>
2017-04-11 02:25:44 +0000

    create the img download function and connect it to the main function

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,49 @@
 var request = require('request');
+var fs = require('fs');
 var GITHUB_USER = "raz346";
 var GITHUB_TOKEN = "b086bcc6763f5fbb966f33c90ec5f24e7a2dd019";
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  //  define user-agent 
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  //  define user-agent
   var requestOption = {
 
-    headers : {
-      "user-agent" : "Ahmed"
+    headers: {
+      "user-agent": "Ahmed"
     },
-    url : requestURL 
-  }
+    url: requestURL
+  };
   // handle error
-  request.get(requestOption,cb)
-       .on('error', function (err) {                                    
+  request.get(requestOption, cb)
+       .on('error', function (err) {
          console.log(err);
        })
       //  handle the data
-      .on('response', function (response) {                         
-       })
+      .on('response', function (response) {
+        return response;
+      });
 }
 getRepoContributors("jquery", "jquery", function(err, result) {
    // get data as  object ??
   var tempResult = JSON.parse(result.body);
   // iterate over the results
   tempResult.forEach(function (element){
-    // print each avatar
-    console.log(element["avatar_url"]);
+    // invoke  downloadImageByURL and pass avatar url and loging info as arguments
+    downloadImageByURL(element["avatar_url"], element["login"] + ".jpg");
   });
 });
+function downloadImageByURL(url, filePath) {
+  console.log('Downloading image...');
+  var path = "./avatars";
+  if(!fs.existsSync(path)){
+    fs.mkdirSync(path);
+  }
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         return response;
+       })
+       .on('end', function(){})
+       .pipe(fs.createWriteStream(path + "/" + filePath));
+}
